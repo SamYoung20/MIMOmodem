f8b41bea59821953e12e1abfddb1fd9fa3f631c0
SamYoung20 <samantha.young@students.olin.edu>
2017-12-07 15:36:13 -0500

    adding xcorrelational and new sample data

--- demodulate2.m
+++ demodulate2.m
@@ -1,17 +1,24 @@
-function y = demodulate(signal, carrierFreq, samplerate)
+function y = demodulate2(signal, carrierFreq, samplerate)
     signalLen = length(signal);
     signalTime = signalLen/samplerate;
     
     t = linspace(0,signalTime,signalLen);
     avWind = (100/carrierFreq)/(1/samplerate);
     phi = 0;
-    for k = (avWind+1):length(signal)
+    y1 = 0;
+    avWind
+    for k = (avWind+1):avWind:length(signal)
+        disp('hi')
         yt1 = cos(carrierFreq*t + phi).*signal';
         y1 = mean(yt1(k-avWind:k));
         yt2 = -sin(carrierFreq*t + phi).*signal';
         y2 = mean(yt2(k-avWind:k));
         phi = 4*y1*y2 + phi;
     end
-    y = cos(carrierFreq*t+phi).*signal';
+    y_raw = signal.*cos(carrierFreq*t + phi)';
+    wc = .5*(carrierFreq * 2* pi)/samplerate;
+    n = linspace(-1000,1000,2000);
+    h = wc/pi * sinc(wc*n/pi);
+    y = conv(y_raw', h, 'same');
 
 end
