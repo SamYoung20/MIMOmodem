922ed7a256e0ebaad710dc7a5f3b6893d5ce8abd
Henry Rachootin <henry.rachootin@students.olin.edu>
2017-12-09 21:01:53 -0500

    noise.mat

--- sendHCalibration.m
+++ sendHCalibration.m
@@ -1,15 +1,8 @@
 [padding,] = makeCarrier(440,48000,.2);
 [carrier,carrierComplex] = makeCarrier(440,48000,1);
-noise = rand(1,length(carrier))*2-1;
-testSound = carrier.*noise;
-
-windowSize = 5000;
-b = (1/windowSize)*ones(1,windowSize);
-a = 1;
-
-plot(filter(b,a,testSound));
-tone = [padding testSound padding];
+load noise.mat
+tone = [testSound];
 z    = zeros(1, length(tone));
-totalTest = [z; tone];
-size(totalTest);
+delay = zeros(1,48000/2);
+totalTest = [z delay tone; tone delay z];
 sound(totalTest,48000);
