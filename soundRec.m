f83a525d22236348fe6d164f5564716e1d163d35
SamYoung20 <samantha.young@students.olin.edu>
2017-12-09 20:54:47 -0500

    adding MIMO

--- soundRec.m
+++ soundRec.m
@@ -1,7 +1,9 @@
-recObj = audiorecorder(44000,8,1)
+recObj = audiorecorder(44000,8,2)
 disp('Start speaking.')
 recordblocking(recObj, 4);
 disp('End of Recording.');
 play(recObj);
 y = getaudiodata(recObj);
-plot(y);
+plot(y(:,1));
+hold on
+plot(y(:,2));
